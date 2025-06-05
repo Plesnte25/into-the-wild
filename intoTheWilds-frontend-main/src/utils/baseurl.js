3ec6dd36ba69a3dd7dc3e3d1f790881ae32ee7fb
Sharjeel <plesnte.workspace@gmail.com>
2025-06-05 23:22:46 +0530

    updated

--- intoTheWilds-frontend-main/src/utils/baseurl.js
+++ intoTheWilds-frontend-main/src/utils/baseurl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// export const BASE_URL = import.meta.env.VITE_API_BASE_URL;   // <-- NEW
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;   // <-- NEW
 
 // const api = axios.create({
 //   baseURL: BASE_URL,
