bf0dedfcb8dbc41f1f927597b1eaa6ca66897600
Sharjeel <plesnte.workspace@gmail.com>
2025-06-05 15:10:19 +0530

    Render API

--- intoTheWilds-frontend-main/src/utils/baseurl.js
+++ intoTheWilds-frontend-main/src/utils/baseurl.js
@@ -1,7 +1,3 @@
-// // export const BASE_URL = "https://intothewilds-backend.onrender.com/api/v1";
-// export const BASE_URL = "https://intothewilds-backend-ghye.onrender.com/api/v1";
-// //  export const BASE_URL = "http://localhost:5000/api/v1";
-
 import axios from "axios";
 
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL;   // <-- NEW
